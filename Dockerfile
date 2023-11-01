89aaf8e7ef2d8d52bcd04797c08f29df06d56d28
KYLE GOOD <kyle.good@creditgenie.com>
2023-11-01 00:21:42 -0400

    initial

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+# Use an official Python runtime as a parent image
+FROM python:3.11.3-alpine
+
+# Set the working directory in the container to /app
+WORKDIR /app
+
+# Install Poetry
+# RUN pip install poetry
+
+# Copy only the necessary files for installing dependencies
+# COPY pyproject.toml poetry.lock* /app/
+
+# Disable virtual environments creation by Poetry
+# as Docker itself provides isolation
+# RUN poetry config virtualenvs.create false
+
+# Install dependencies using Poetry
+# RUN poetry install --no-dev
+
+# Copy the rest of your application's code
+# COPY . /app
+
+# Make port 80 available to the world outside this container
+EXPOSE 80
+
+# Define environment variable
+ENV NAME World
+
+# Run app.py when the container launches
+CMD ["python", "app.py"]
---
+++ backend/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+import toml
+import uvicorn
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+app = FastAPI()
+
+
+
+def get_predictions_route(model_name: str, model_config: file, prediction_date: date):
+    @app.get(f"/predictions/{model_name}/{date}")
+    async def get_predictions():
+
+        return {"model": model_name, "data:" "Model prediction"}
+    
+def load_config():
+    with open("model_configs.yaml", "r") as file:
+        config = yaml.safe_load(file)
+        for model_name, model_config in config.items():
+            get_predictions_route(model_name, model_config)
+
+class ConfigFileChangeHandler(FileSystemEventHandler):
+    def on_modification(self, event):
+        if event.src_path.endswith("config.yaml"):
+            load_config()
+
+observer = Observer()
+observer.schedule(ConfigFileChangeHandler(), path='.', recursive=False)
+observer.start()
+
+load_config()
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)
